8ec164b16ab1260cf409254147b4fac10bd44d5a
comit <alireza2040afshari@gmail.com>
2026-06-06 01:09:21 +0330

    learn thinker

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print(1)
+from tkinter import Tk, filedialog
+from pydub import AudioSegment
+
+Tk().withdraw()
+
+input_file = filedialog.askopenfilename(
+    title="انتخاب فایل صوتی"
+)
+
+output_file = filedialog.asksaveasfilename(
+    title="ذخیره فایل MP3",
+    defaultextension=".mp3",
+    filetypes=[("MP3 files", "*.mp3")]
+)
+
+audio = AudioSegment.from_file(input_file)
+audio.export(output_file, format="mp3")
+
+print("تمام شد")
