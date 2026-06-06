e324cb69972a96f7921602aa5a8f5d7615f7161a
comit <alireza2040afshari@gmail.com>
2026-06-06 23:43:42 +0330

    add test

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from pydub import AudioSegment
 Tk().withdraw()
 
 input_file = filedialog.askopenfilename(
-    title="انتخاب فایل صوتی"
+    title = "choose mp3 file"
 )
 
 output_file = filedialog.asksaveasfilename(
-    title="ذخیره فایل MP3",
-    defaultextension=".mp3",
+    title = "save mp3",
+    defaultextension= ".mp3",
     filetypes=[("MP3 files", "*.mp3")]
 )
 
 audio = AudioSegment.from_file(input_file)
 audio.export(output_file, format="mp3")
 
-print("تمام شد")
+print("the end")
+if __name__ == "__main__"_:
+    
