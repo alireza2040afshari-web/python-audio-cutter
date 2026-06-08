4898d8fad296b2c0b2fc47e6f36584ea25f427ab
comit <alireza2040afshari@gmail.com>
2026-06-08 22:39:00 +0330

    choose file complete

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import tkinter as tk 
-from tkinter import tk
+from tkinter import filedialog
 
 
 def main() -> None:
     root = tk.Tk()
     root = root.withdraw()
+    file_path = filedialog.askopenfilename(
+        title="choose MP3",
+        filetypes=[("MP3 Files", "*.mp3")]
+    )
  
 
 if __name__ == "__main__":
